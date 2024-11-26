740f7b0478ca69d6af8aab6cf213820f23517773
zujkovicstasa <zujkovicstasa@gmail.com>
2024-11-26 14:22:22 +0100

    project init

---
+++ cocktails9/cocktails9/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  cocktails9
+//
+//  Created by Stasa Zujkovic on 26.11.24..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ cocktails9/cocktails9/cocktails9App.swift
@@ -0,0 +1,17 @@
+//
+//  cocktails9App.swift
+//  cocktails9
+//
+//  Created by Stasa Zujkovic on 26.11.24..
+//
+
+import SwiftUI
+
+@main
+struct cocktails9App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
