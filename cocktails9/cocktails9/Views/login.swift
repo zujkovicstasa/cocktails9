4522f36cdc1df1451f29b99ae798da4db92afc7e
zujkovicstasa <zujkovicstasa@gmail.com>
2024-11-27 13:35:11 +0100

    Login View bez navigacije

---
+++ cocktails9/cocktails9/Views/login.swift
@@ -0,0 +1,18 @@
+//
+//  login.swift
+//  cocktails9
+//
+//  Created by Stasa Zujkovic on 26.11.24..
+//
+
+import SwiftUI
+
+struct login: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    login()
+}
