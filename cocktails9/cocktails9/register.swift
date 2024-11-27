b9cf03903a1159fbb6ff53be3f95e5356205d3f7
zujkovicstasa <zujkovicstasa@gmail.com>
2024-11-27 15:11:32 +0100

    Register v1

---
+++ cocktails9/cocktails9/register.swift
@@ -0,0 +1,18 @@
+//
+//  register.swift
+//  cocktails9
+//
+//  Created by Stasa Zujkovic on 27.11.24..
+//
+
+import SwiftUI
+
+struct register: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    register()
+}
