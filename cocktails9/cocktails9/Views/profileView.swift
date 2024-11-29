928006f9a7d9dba5166f09148ed9c9745fdb9d94
zujkovicstasa <zujkovicstasa@gmail.com>
2024-11-29 09:41:29 +0100

    mainTab v1

---
+++ cocktails9/cocktails9/Views/profileView.swift
@@ -0,0 +1,18 @@
+//
+//  profileView.swift
+//  cocktails9
+//
+//  Created by Stasa Zujkovic on 28.11.24..
+//
+
+import SwiftUI
+
+struct profileView: View {
+    var body: some View {
+        Text("Profile")
+    }
+}
+
+#Preview {
+    profileView()
+}
--- cocktails9/cocktails9/mainTabView.swift
+++ cocktails9/cocktails9/mainTabView.swift
@@ -8,30 +8,101 @@
 import SwiftUI
 
 struct mainTabView: View {
+    
+    enum Tab {
+           case cocktails
+           case favorites
+           case profile
+       }
+    
+    @State var selectedTab: Tab = .cocktails
+    
     var body: some View {
         
-        TabView{
-            Text("Cocktails")
-                .tabItem({
-                    Image(systemName:"1.square.fill")
-                    Text("Cocktails")
+        ZStack(alignment: .bottom){
+            
+            Group{
+                
+                switch selectedTab {
                     
-                })
-            Text("Favorites")
-                .tabItem({
-                    Image(systemName:"2.square.fill")
-                    Text("Favorites")
-                    
-                })
-            Text("Profile")
-                .tabItem({
-                    Image(systemName:"3.square.fill")
-                    Text("Profile")
-                    
-                })
+                case .cocktails:
+                    cocktailsView()
+                case .favorites:
+                    favoritesView()
+                case .profile:
+                    profileView()
+                }
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            
+            HStack{
+                
+                Button{
+                    selectedTab = .cocktails
+                } label: {
+                    VStack(spacing:0){
+                        Image(systemName: "wineglass")
+                            .frame(width:44, height:29)
+                        Text("Cocktails")
+                            .font(.caption2)
+                            .lineLimit(1)
+                    }
+                    .frame(maxWidth: .infinity)
+                }
+                .foregroundStyle(selectedTab == .cocktails ? .primary : .secondary)
+                
+                Button{
+                    selectedTab = .favorites
+                } label: {
+                    VStack(spacing:0){
+                        Image(systemName: "heart")
+                            .frame(width:44, height:29)
+                        Text("Favorites")
+                            .font(.caption2)
+                            .lineLimit(1)
+                    }
+                    .frame(maxWidth: .infinity)
+                }
+                .foregroundStyle(selectedTab == .favorites ? .primary : .secondary)
+                Button{
+                    selectedTab = .profile
+                } label: {
+                    VStack(spacing:0){
+                        Image(systemName: "person")
+                            .frame(width:44, height:29)
+                            
+                        Text("Profile")
+                            .font(.caption2)
+                            .lineLimit(1)
+                    }
+                    .frame(maxWidth: .infinity)
+                }
+                .foregroundStyle(selectedTab == .profile ? .primary : .secondary)
+                
+            }
+            
         }
+        .padding(.horizontal, 8)
+        .padding(.top, 14)
+        .frame( height: 88, alignment: .top)
         .edgesIgnoringSafeArea(.top)
         .accentColor(Color.primary)
+        .background(.ultraThinMaterial, in:
+            RoundedRectangle(cornerRadius: 34))
+        .background(
+            HStack {
+                Spacer()
+                
+                Circle().fill(.pink).frame(width:80)
+                Spacer()
+            }
+            .padding(.horizontal, 8 )
+            
+        )
+        .frame(maxHeight: . infinity, alignment: .bottom)
+        .ignoresSafeArea(edges: .bottom)
+        
+        
     }
 }
 
