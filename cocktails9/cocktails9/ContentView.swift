b9ddc5bc758baf538fc5c838cd33a57179122915
zujkovicstasa <zujkovicstasa@gmail.com>
2024-12-03 14:58:24 +0100

    Splash screen popravljen ContentView

--- cocktails9/cocktails9/ContentView.swift
+++ cocktails9/cocktails9/ContentView.swift
@@ -1,15 +1,13 @@
-//
-//  ContentView.swift
-//  cocktails9
-//
-//  Created by Stasa Zujkovic on 26.11.24..
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @State private var navigateToMain = false
+    
     var body: some View {
-        Text("Content View")
+        SplashView(navigateToMain: $navigateToMain)
+            .fullScreenCover(isPresented: $navigateToMain) {
+                MainContentView()
+            }
     }
 }
 
--- cocktails9/cocktails9/cocktails9App.swift
+++ cocktails9/cocktails9/cocktails9App.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct cocktails9App: App {
     var body: some Scene {
         WindowGroup {
-            ViewCoordinator()
+            ContentView()
         }
     }
 }
